25a89ed4c089bad5c96d6b65b4fda2cc69b39527
harryscully <harry.scully@ugogroup.co.uk>
2025-05-15 13:59:30 +0100

    Initialise repo file structure

---
+++ script.js
@@ -0,0 +1,9 @@
+// 01. Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
+
+// 02. Your game will be played by a human player. You will write a function that takes the user choice and returns it.
+
+// 03. Your game will keep track of the players score. You will write variables to keep track of the players score.
+
+// 04. Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
+
+// 05 Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
